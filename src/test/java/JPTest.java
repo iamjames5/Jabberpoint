03c23d814f93d6e6e37126f2be733899b46206a9
ZehuiWangTeddy <111961563+ZehuiWangTeddy@users.noreply.github.com>
2024-03-21 00:18:32 +0100

    add a simple test

---
+++ src/test/java/JPTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+
+public class JPTest
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue(true);
+    }
+
+}
