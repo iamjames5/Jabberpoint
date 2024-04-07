8dc37d1b70402f675ba07d4482ce831da95c47b9
ZehuiWangTeddy <111961563+ZehuiWangTeddy@users.noreply.github.com>
2024-04-07 18:56:46 +0200

    Reformat code

--- src/test/java/com/nhlstenden/JabberPoint/SlideItemManagement/BitMapItemTest.java
+++ src/test/java/com/nhlstenden/JabberPoint/SlideItemManagement/BitMapItemTest.java
@@ -1,27 +1,29 @@
 package com.nhlstenden.JabberPoint.SlideItemManagement;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
 public class BitMapItemTest {
 
-    @Test
-    public void testConstructor_withValidFile_shouldCreateBitMapItem() {
-        BitMapItem bitmapItem = new BitMapItem(1, "test");
+  @Test
+  public void testConstructor_withValidFile_shouldCreateBitMapItem() {
+    BitMapItem bitmapItem = new BitMapItem(1, "test");
 
-        assertNotNull(bitmapItem);
-        assertEquals(1, bitmapItem.getLevel());
-        assertEquals("test", bitmapItem.getName());
-    }
-    @Test
-    public void testConstructor_withInvalidFile_shouldCreateBitMapItemWithNullBufferedImage() {
-        BitMapItem bitmapItem = new BitMapItem(1, "invalid/test");
+    assertNotNull(bitmapItem);
+    assertEquals(1, bitmapItem.getLevel());
+    assertEquals("test", bitmapItem.getName());
+  }
 
-        assertNotNull(bitmapItem);
+  @Test
+  public void testConstructor_withInvalidFile_shouldCreateBitMapItemWithNullBufferedImage() {
+    BitMapItem bitmapItem = new BitMapItem(1, "invalid/test");
 
-        assertEquals(1, bitmapItem.getLevel());
-        assertEquals("invalid/test", bitmapItem.getName());
+    assertNotNull(bitmapItem);
 
-        assertNull(bitmapItem.getBufferedImage());
-    }
+    assertEquals(1, bitmapItem.getLevel());
+    assertEquals("invalid/test", bitmapItem.getName());
+
+    assertNull(bitmapItem.getBufferedImage());
+  }
 }
